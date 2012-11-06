776fab833c5c0ca6a008d125d57373a4b222e94e
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-11-06 18:58:54 -0500

    add documentation

---
+++ R/package.R
@@ -0,0 +1,6 @@
+#' slidifyLibraries contains external dependencies required by slidify
+#' 
+#' @docType package
+#' @name slidifyLibraries
+#' @author Ramnath Vaidyanathan <\url{http://www.slidify.org}>
+NULL
